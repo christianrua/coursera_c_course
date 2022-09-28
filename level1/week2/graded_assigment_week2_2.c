052038ce296d6fb8d26a1f0b8fd963e1f600bb63
christian rua <christian.rua@autodesk.com>
2022-09-27 20:36:40 -0500

    adding content from week3

--- level1/week2/graded_assigment_week2_2.c
+++ level1/week2/graded_assigment_week2_2.c
@@ -1,3 +1,7 @@
+/* Print the sin value for a given value between 0 and 1 non inclusive
+ * Christian Rua
+ * September 27 2022
+ * */
 #include <stdio.h>
 #include <math.h>
 
@@ -5,12 +9,16 @@
 
 int main () {
 
-   double value, response, val;
-   printf("Enter value:");
+   double value, response, constant;
+   printf("Enter value between 0 and 1, non inclusive: ");
    scanf("%lf", &value);
-   val = PI / 180;
-   response = sin(value*val);
-   printf("The sine of %lf is %lf degrees", value, response);
-   
-   return(0);
+   if (value < 0 || value > 1) {
+      printf("The input value must be in the range 0 to 1, non inclusive \n");
+      return(0);
+   } else {
+      constant = PI / 180;
+      response = sin(value*constant);
+      printf("The sine of %lf is %lf degrees\n", value, response);
+      return(0);
+   }
 }
