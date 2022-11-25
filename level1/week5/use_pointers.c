712aa433ec1868da1389c8179a24931ea1adbb43
christian.rua@gmail.com <christian.rua@gmail.com>
2022-11-24 19:42:53 -0500

    adding week5 content

---
+++ level1/week5/use_pointers.c
@@ -0,0 +1,35 @@
+/*
+    Use of pointers
+    Christian Rua Nov 24, 2022
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int grades[5] = {78,67,92,83,88};
+    double sum = 0.0;
+    double *ptr_to_sum = &sum;
+    int i;
+
+    printf("\n my grades are:\n");
+
+    for(i = 0; i < 5; i++){
+        printf("%d\t", grades[i]);
+    }
+
+    printf("\n\n");
+
+    for(i = 0; i< 5; i++){
+        sum = sum + grades[i];
+    }
+
+    printf("my average is %.2f\n\n", sum/5);
+    printf("\n\n");
+
+    printf("sum is at %p, or %ul and is %lf\n",
+            ptr_to_sum, 
+            ptr_to_sum,
+            *ptr_to_sum);
+    printf("grades are at %lu to %lu\n", grades, grades + 5);      
+    return 0;  
+}
